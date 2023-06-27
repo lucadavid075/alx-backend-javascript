3aae79f5e0d7f93e1f834304ae3b614128936282
Luke David <daiveedlucas049@gmail.com>
2023-06-27 05:14:16 +0000

    [fix] eslint

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,10 +1,14 @@
 export default function createIteratorObject(report) {
   return {
-    *[Symbol.iterator]() {
+    * [Symbol.iterator]() {
       for (const department in report.allEmployees) {
-        const employees = report.allEmployees[department];
-        for (const employee of employees) {
-          yield employee;
+        if (Object.prototype.hasOwnProperty.call(report.allEmployees, department)) {
+          const employees = report.allEmployees[department];
+          for (const employee in employees) {
+            if (Object.prototype.hasOwnProperty.call(employees, employee)) {
+              yield employees[employee];
+            }
+          }
         }
       }
     },
