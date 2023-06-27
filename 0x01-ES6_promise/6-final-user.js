197654e26bf0dfd69a2275999034e1b483b28b97
Luke David <daiveedlucas049@gmail.com>
2023-06-27 16:19:17 +0000

    [fix] used allSettled instead of aall methods

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([
+  return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName).catch((error) => error),
   ]).then((values) => {
