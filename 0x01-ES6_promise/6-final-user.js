869e83a12fc2fdfb3d035c2aaf147c8b4f4954f1
Luke David <daiveedlucas049@gmail.com>
2023-06-27 16:07:18 +0000

    fix 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,9 +4,7 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.all([
     signUpUser(firstName, lastName),
-    uploadPhoto(fileName).catch((error) => {
-      return error;
-    }),
+    uploadPhoto(fileName).catch((error) => error),
   ]).then((values) => {
     let status;
 
