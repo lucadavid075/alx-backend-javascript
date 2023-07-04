3efefd3582540f60981d3ac5069af2af0748950f
Luke David <daiveedlucas049@gmail.com>
2023-07-04 20:07:31 +0000

    :bug: fix get ids sum  function

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,15 +1,15 @@
 /**
- * 
- * @param {*} studentList 
+ *
+ * @param {*} studentList
  * @returns  the sum of all the student ids
  */
 
 const getStudentIdsSum = (studentList) => {
-    if (!Array.isArray(studentList)) {
-        return []
-    }
+  if (!Array.isArray(studentList)) {
+    return [];
+  }
 
-    return studentList.reduce((accumulator, current) => accumulator + current.id, 0)
-}
+  return studentList.reduce((accumulator, current) => accumulator + current.id, 0);
+};
 
 export default getStudentIdsSum;
