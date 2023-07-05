d3c8d3a48e7fdccdf481a552bc5d1cd930cb27cb
Luke David <daiveedlucas049@gmail.com>
2023-07-05 03:14:39 +0000

    :bug: still bugging me

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -10,19 +10,18 @@ const MAX_ENDPOINT_CALLS = 5;
 
 /**
  *
- * @param {{
- * protocol: 'http',
- * name: 'getUsers',
- * }} endpoint - endpoint to be queried
+ * @param {
+ * protocol: 'http'(String),
+ * name: 'getUsers'(String),
+ * } endpoint - endpoint to be queried
  */
 
 export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 0);
   }
-  const count = weakMap.get(endpoint);
-  weakMap.set(endpoint, count + 1);
-  if (count >= MAX_ENDPOINT_CALLS) {
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
     throw new Error('Endpoint load is high');
   }
 }
